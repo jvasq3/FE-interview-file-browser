eb9665b4573ec76d1f2655355533734819f12a4d
Juan Vasquez <juanvasqueziii@Juans-MBP.attlocal.net>
2021-05-17 10:10:29 -0500

    clean

--- client/src/components/RangeInput.jsx
+++ client/src/components/RangeInput.jsx
@@ -14,49 +14,49 @@ export default (props) => {
 
     return (
         <Box>
-                <Chip 
-                  color="primary" 
-                  onDelete={() => onDelete('gt')} 
-                  label={
+            <Chip
+                color="primary"
+                onDelete={() => onDelete('gt')}
+                label={
                     <Box>
-                      <strong>{label} &gt;</strong>
-                      <input 
-                        // onChange={(e) => setSizeGt(Number(e.currentTarget.value))} 
-                        onChange={e => onChange(Number(e.target.value), 'gt')}
-                        type="number"
-                        value={sizeGt}
-                        style={{
-                          marginLeft: 8,
-                          background: 'transparent',
-                          color: 'white',
-                          border: 'none',
-                          width: 80,
-                        }}
-                      />
+                        <strong>{label} &gt;</strong>
+                        <input
+                            // onChange={(e) => setSizeGt(Number(e.currentTarget.value))} 
+                            onChange={e => onChange(Number(e.target.value), 'gt')}
+                            type="number"
+                            value={sizeGt}
+                            style={{
+                                marginLeft: 8,
+                                background: 'transparent',
+                                color: 'white',
+                                border: 'none',
+                                width: 80,
+                            }}
+                        />
                     </Box>
-                  }
-                />
-                <Chip 
-                  color="primary" 
-                  onDelete={() => onDelete('lt')} 
-                  label={
+                }
+            />
+            <Chip
+                color="primary"
+                onDelete={() => onDelete('lt')}
+                label={
                     <Box>
-                      <strong>{label} &lt;</strong>
-                      <input 
-                        onChange={e => onChange(Number(e.target.value), 'lt')}
-                        type="number"
-                        value={sizeLt}
-                        style={{
-                          marginLeft: 8,
-                          background: 'transparent',
-                          color: 'white',
-                          border: 'none',
-                          width: 80,
-                        }}
-                      />
+                        <strong>{label} &lt;</strong>
+                        <input
+                            onChange={e => onChange(Number(e.target.value), 'lt')}
+                            type="number"
+                            value={sizeLt}
+                            style={{
+                                marginLeft: 8,
+                                background: 'transparent',
+                                color: 'white',
+                                border: 'none',
+                                width: 80,
+                            }}
+                        />
                     </Box>
-                  }
-                />
-              </Box>
+                }
+            />
+        </Box>
     );
 }
